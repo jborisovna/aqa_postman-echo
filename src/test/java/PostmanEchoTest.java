15c13bce1172e3545ec446032f373c3e0295611a
jenna <ravennasw@gmail.com>
2023-06-25 16:58:36 +0700

    break the code

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -18,7 +18,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("rain"))
+                .body("data", equalTo("sun"))
         ;
     }
 }
